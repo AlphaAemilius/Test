5dad28718f2316c6184a41bd0c2f3aa03ccf2094
AlphaAemilius <19005768@vcconnect.co.za>
2021-03-04 11:37:37 +0200

    First one

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Me");
+        }
+    }
+}
